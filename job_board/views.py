9039b44d241c3bd223346830d5743a98d2125957
Andrei <andrei.baditoiu@gmail.com>
2024-10-03 22:58:13 +0300

    Modified: index, job_board/Templates/job_detail (html), urls & views(py). Added _base.html and styling.

--- job_board/views.py
+++ job_board/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import HttpResponse, render
+from django.shortcuts import render, get_object_or_404
 
 from job_board.models import JobPosting
 
@@ -10,3 +10,18 @@ def index(request):
     context = {'job_postings': active_postings
                }
     return render(request, 'index.html', context)
+
+
+# own
+# def job_detail(request, job_posting_id):
+#     job_posting = JobPosting.objects.get(pk=job_posting_id)
+#     context = {'job_posting': job_posting_id}
+#     return render(request, 'job_detail.html', context)
+#
+# # The one below is more explanatory
+def job_detail(request, pk):
+    job_posting = get_object_or_404(JobPosting, pk=pk, is_active=True)
+    context = {
+        'job_posting': job_posting
+    }
+    return render(request, 'job_detail.html', context)
